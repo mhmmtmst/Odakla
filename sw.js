e3959cfecdafd96193943f32eb62412afb7461f2
Muhammet Nihat MESUT <mhmmtmesut@gmail.com>
2026-07-11 17:50:55 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'odakla-v1';
+const CORE_ASSETS = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', function(event){
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache){
+      return cache.addAll(CORE_ASSETS).catch(function(){});
+    })
+  );
+});
+
+self.addEventListener('activate', function(event){
+  event.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(keys.filter(function(k){ return k !== CACHE_NAME; }).map(function(k){ return caches.delete(k); }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event){
+  if(event.request.method !== 'GET') return;
+  var url = event.request.url;
+  // Never cache Firebase/Google API calls - always go to network for live data
+  if(url.indexOf('googleapis.com') !== -1 || url.indexOf('gstatic.com') !== -1 || url.indexOf('firebaseapp.com') !== -1){
+    return;
+  }
+  event.respondWith(
+    caches.match(event.request).then(function(cached){
+      var networkFetch = fetch(event.request).then(function(response){
+        if(response && response.status === 200){
+          var clone = response.clone();
+          caches.open(CACHE_NAME).then(function(cache){ cache.put(event.request, clone); });
+        }
+        return response;
+      }).catch(function(){ return cached; });
+      return cached || networkFetch;
+    })
+  );
+});
